f69178104ad6e7c7a42e9bd106f971773b653a39
Faisal Awan <faisalawan.live@gmail.com>
2015-09-08 12:09:29 +0500

    Get Twitter Initial Commit

---
+++ get-twitter.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Get Twitter
+Description: Plugin to get your tweets in your Wordpress Website
+Plugin URI: http://#
+Author: Faisal Awan
+Author URI: https://github.com/Faisalawanisee
+Version: 1.0
+Text Domain: get_twitter
+Domain Path: /language
+*/
+
+/*
+
+    Copyright (C) Year  Author  Email
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+;
