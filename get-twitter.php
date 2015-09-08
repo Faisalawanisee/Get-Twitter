afe78b86f07a2bc6097d692c29f4f566823cf995
Faisal Awan <faisalawan.live@gmail.com>
2015-09-08 12:19:22 +0500

    Add License GPL2 in plugin file

--- get-twitter.php
+++ get-twitter.php
@@ -6,6 +6,21 @@ Plugin URI: https://github.com/Faisalawanisee/Get-Twitter
 Author: Faisal Awan
 Author URI: https://github.com/Faisalawanisee
 Version: 1.0
+License:     GPL2
 Text Domain: get_twitter
 Domain Path: /language
+*/
+/*
+Get Twitter is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Get Twitter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Get Twitter. If not, see https://github.com/Faisalawanisee/Get-Twitter.
 */
